79b1626dade592838acedd9859d9abfb748e471c
BorisYermolenko <ermolenko802@gmail.com>
2025-11-04 17:50:34 +0500

    feat: внес изменения. Изменил название класса Program на GameService. Поменял сигнатуру метода makeGuess() в классе Player. В классе Game генерацию значения для поля targetNumber вынес в отдельный метод

--- src/model/Game.java
+++ src/model/Game.java
@@ -1,4 +1,27 @@
 package model;
 
 public class Game {
+
+    private int targetNumber;
+
+    public Game() {
+        targetNumber = createNumber();
+    }
+
+    public int createNumber() {
+        return (int) (Math.random() * 10) + 1;
+    }
+
+    public String guess(int number) {
+        String result;
+        if (targetNumber == number) {
+            result = "угадал";
+        } else if (number > targetNumber) {
+            result = "меньше";
+        } else {
+            result = "больше";
+        }
+        return result;
+    }
+
 }
--- src/model/Player.java
+++ src/model/Player.java
@@ -1,4 +1,16 @@
 package model;
 
+import java.util.Scanner;
+
 public class Player {
+    private final Scanner input = new Scanner(System.in);
+
+    public int makeGuess() {
+        if (input.hasNextInt()) {
+            return input.nextInt();
+        } else {
+            return makeGuess();
+        }
+    }
+
 }
